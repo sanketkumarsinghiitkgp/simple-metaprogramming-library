4103a681d8ccf4d9e8df15d7f03f6162da4617da
Sanket Kumar Singh <onlyforcodeforces@gmail.com>
2023-08-27 02:52:32 +0530

    create compile time type list and add the length and append functionality.

---
+++ src/type_list.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstddef>
+namespace tml
+{
+	template <typename... Ts>
+	struct type_list {};
+
+	template <typename T>
+	struct length;
+
+	template <>
+	struct length<type_list<>>
+	{
+		static constexpr std::size_t value = 0;
+	};
+
+	template <typename T, typename... Ts>
+	struct length<type_list<T,Ts...>>
+	{
+		static constexpr std::size_t value = 1+length<type_list<Ts...>>::value;
+	};
+
+	template <typename TypeList, typename T>
+	struct append;
+	
+	template <typename... Ts, typename T>
+	struct append<type_list<Ts...>,T>
+	{
+		using type = type_list<Ts...,T>;
+	};
+}
